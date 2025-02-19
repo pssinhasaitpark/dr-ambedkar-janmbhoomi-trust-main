45c2be9bfab3eaf1e80563dfdb1ceb9407fc96b1
Satya <satya.jain@parkhya.net>
2025-02-19 15:52:50 +0530

    Added env variables

--- src/view/redux/axios/axios.js
+++ src/view/redux/axios/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.0.127:8080/api', 
+  baseURL: `${process.env.REACT_APP_BASE_URL}`,
   headers: {
     'Content-Type': 'application/json',
   },
